76645dbade1f1a6cedf8be5dcc77f5798c54f368
Christoph Burschka <christoph@burschka.de>
2018-07-10 22:22:00 +0200

    Add command-line arguments.

--- getdata.py
+++ getdata.py
@@ -1,11 +1,25 @@
-import names
+#!/usr/bin/env python3
+
+import ssa_names
 import json
+import argparse
+import datetime
+import sys
 
-data = {}
+parser = argparse.ArgumentParser()
 
-def main():
-    for year in range(1880, 2018):
-        data[year] = names.get_year(year)
-    print(json.dumps(data, indent=4))
+parser.add_argument("start", type=int, default=1880, help="First year to acquire")
+parser.add_argument("end", type=int, default=datetime.datetime.now().year - 1, help="Last year to load")
+parser.add_argument("--output", "-o", default=sys.stdout, help="Where to write the JSON data.")
 
-main()
+args = parser.parse_args()
+
+def main(args):
+    data = {}
+    for year in range(args.start, args.end + 1):
+        data[year] = ssa_names.get_year(year)
+    if type(args.output) is str:
+        args.output = open(args.output, 'w')
+    json.dump(data, args.output, indent=4)
+
+main(args)
--- metaculus-1060.py
+++ metaculus-1060.py
@@ -1,10 +1,26 @@
-import json
+#!/usr/bin/env python3
 
-data = json.load(open('data.json'))
-data = {int(year): names for year, names in data.items()}
-interval = 30
-for year, names in data.items():
-    if year + interval in data:
-        names2 = data[year + interval]
-        common = (set(names['f']) & set(names2['f'])) | (set(names['m']) & set(names2['m']))
-        print(year, year+interval, len(common), *common)
+import json
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description="Check for overlap in names over a given interval.")
+
+parser.add_argument("--input", default=sys.stdin, help="Input file (default STDIN)")
+parser.add_argument("--interval", type=int, default=30, help="Interval length")
+parser.add_argument("--all", action="store_const", const=True, help="Check all years in interval, not just ends.")
+
+def main(args):
+    if type(args.input) is str:
+        args.input = open(args.input, "r")
+    data = json.load(args.input)
+    data = {int(year): names for year, names in data.items()}
+    interval = args.interval
+    for year, names in data.items():
+        if year + interval in data:
+            x = range(year, year+interval+1) if args.all else (year, year+interval)
+            c = lambda g: set.intersection(*(set(data[i][g]) for i in x))
+            common = c('m') | c('f')
+            print(year, year+interval, len(common), *common)
+
+main(parser.parse_args())
