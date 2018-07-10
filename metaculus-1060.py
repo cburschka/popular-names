07d773cc18db2fb5c7b17f6d10e2f824f2c94281
Christoph Burschka <christoph@burschka.de>
2018-07-10 18:54:27 +0200

    Dynamic interval.

--- metaculus-1060.py
+++ metaculus-1060.py
@@ -2,9 +2,9 @@ import json
 
 data = json.load(open('data.json'))
 data = {int(year): names for year, names in data.items()}
-
+interval = 30
 for year, names in data.items():
-    if year + 30 in data:
-        names2 = data[year + 30]
+    if year + interval in data:
+        names2 = data[year + interval]
         common = (set(names['f']) & set(names2['f'])) | (set(names['m']) & set(names2['m']))
-        print(year, year+30, len(common), *common)
+        print(year, year+interval, len(common), *common)
